e2790b3fbff66261f75125c989fca71e0f524b17
fesfsasf <ADS7706879@gmail.com>
2025-02-05 16:16:35 +0800

    添加按键中断服务函数和LED控制，优化外设初始化

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,29 @@
 #define DHTTYPE    DHT11     // DHT 11
 //#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+#define LED_Yellow 21
+#define Key_1      48
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
-uint8_t led = 13;  // LED灯引脚
+uint8_t LED_state = 0;
+//  按键中断服务函数
+void Key_state_Service(void) {
+  LED_state = digitalRead(Key_1);
+  if(LED_state){
+    delay(5);
+    digitalWrite(LED_Yellow, LED_state);
+  } else {
+    delay(5);
+    digitalWrite(LED_Yellow, LED_state);
+  }
+}
 
 // 外设初始化
 void setup() {
   Serial.begin(115200);
-  pinMode(led, OUTPUT);
+  while (!Serial); // 拔掉数据线运行，这行记得注释掉，否则不连接串口，会卡在这里，后面代码就无法运行
+  pinMode(LED_Yellow, OUTPUT);
+  attachInterrupt(Key_1, Key_state_Service, CHANGE);    // 为按键引脚设置中断服务函数，在setup函数中调用  dht.begin();       // DHT11初始化 
   dht.begin();       // DHT11初始化 
   Serial.println(F("DHT11 Initialized Completed"));
   // Print temperature sensor details.
@@ -52,4 +67,9 @@ void loop() {
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
 }
